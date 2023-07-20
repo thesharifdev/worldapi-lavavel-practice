47ce501d80afb7fa13e935514a548a3df6398348
thesharifdev <sharif.golang@gmail.com>
2023-07-20 06:14:36 +0600

    post request created and validation added for signup

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,13 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
-    public function signup(){
+    public function signup(Request $request)
+    {
+        $params = ['name', 'email', 'password', 're_password'];
+        
+        $this->validate_request($request, $params);
+
         return [
             'success' => true,
             'message' => 'Hello from signup',
         ];
+    }
+
+    /**
+     * Validate the request
+     * 
+     * @param Request $request
+     * @param array $params
+     * 
+     * @return void
+     */
+    private function validate_request(Request $request, $params)
+    {
+        foreach($request->all() as $key=>$value) {
+            if(!in_array($key, $params)) {
+                Log::alert('Invalid parameter: ' . $key);
+                return;
+            }
+        }
     }
 }
